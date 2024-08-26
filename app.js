a4e08151ebf18aec2a3c0c35828cd405d70822b4
Jhendy Gonzales <jhendy.gonzales@gmail.com>
2024-08-26 17:26:13 -0500

    actualizando app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const encriptar = document.getElementById("button__encriptar");
+const desencriptar = document.getElementById("button__desencriptar");
+const copy = document.getElementById("button__copiar");
+const textoInicial = document.getElementById("textoInput");
+const textFinal = document.getElementById("textoFinal");
+const munheco = document.getElementById("munheco");
+const textInfo = document.getElementById("textoInfo");
+const rigth = document.getElementById("seccion2")
+
+const remplace = (newvalue) => {
+	textFinal.innerHTML = newvalue;
+	textFinal.classList.add("ajustar");
+	rigth.classList.add("ajuste")
+	textoInicial.value = "";
+	textoInicial.style.height = "auto"
+	textoInicial.placeholder = "Ingrese el texto aquí";
+	munheco.classList.add("ocultar");
+	textInfo.classList.add("ocultar");
+	copy.classList.remove("bn_ocultar");
+}
+
+const reset = () => {
+	textoInicial.value = "";
+    textoInicial.style.height = "auto";
+	textFinal.innerHTML = "";
+	rigth.classList.remove("ajuste")
+	textFinal.classList.remove("ajustar");
+	munheco.classList.remove("ocultar");
+	textFinal.placeholder = "Ningún mensaje fue encontrado";
+	textInfo.classList.remove("ocultar")
+	copy.classList.add("bn_ocultar");
+	textoInicial.focus();
+};
+
+let remplazar = [
+	["e", "enter"],
+	["o", "ober"],
+	["i", "imes"],
+	["a", "ai"],
+	["u", "ufat"]
+];
